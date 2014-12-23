1f3fef19341c585874c19711dd8b59446cde7afc
unknown <pandavigoureux29@gmail.com>
2014-12-24 00:38:57 +0100

    kimi collect effect

--- assets/behaviours/items/kimi.js
+++ assets/behaviours/items/kimi.js
@@ -4,6 +4,8 @@
 var Kimi = function(_gameobject) {	
 	LR.Behaviour.Trigger.call(this,_gameobject);
 	this.kimi_id = 0;
+
+	this.cosTemp = LR.Utils.toRadians(90);
 }
 
 Kimi.prototype = Object.create(LR.Behaviour.Trigger.prototype);
@@ -26,8 +28,37 @@ Kimi.prototype.create = function( _data ){
 }
 
 Kimi.prototype.onTriggered = function(_gameobject){
-	if( _gameobject.layer == "player"){
-		this.entity.kill();
+	if( _gameobject.layer == "player" && !this.collected){
+		this.collected = true;
+		this.playerGO = _gameobject;
+		this.go.getShape().sensor = true;
+		this.go.gravity = 0;
 		this.game.playerSave.getActiveLevelSave().kimis.push(this.kimi_id);
+	}
+}
+
+Kimi.prototype.update = function(_gameobject){
+	if(this.collected){
+		this.entity.angle -= this.entity.game.time.elapsed *0.35;
+		if( !this.goToPlayer){
+			this.cosTemp -= 0.0025 * this.entity.game.time.elapsed;
+			var cos = Math.cos(this.cosTemp);
+			this.go.y = this.playerGO.y - cos * 50;
+			if(this.lastCos != null && this.lastCos > cos)
+				this.goToPlayer = true;
+			this.lastCos = cos;
+		}else{
+			var vector = Phaser.Point.subtract(this.playerGO.entity.position,this.entity.position);
+			var speedVector = new Phaser.Point(vector.x, vector.y).normalize();
+			var accelerator = 4 + vector.getMagnitude() *0.02;
+			speedVector.x *= accelerator;
+			speedVector.y *= accelerator;
+			this.go.x += speedVector.x ;
+			this.go.y += speedVector.y ;
+			if(vector.getMagnitude() <= speedVector.getMagnitude() ){
+				this.entity.kill();
+			}
+		}
+		
 	}
 }
