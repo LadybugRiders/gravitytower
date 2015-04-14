3c23bff77fa830f3009fa60354dea37474f56558
pandavigoureux29 <pandavigoureux29@gmail.com>
2015-04-14 17:58:56 +0200

    debug

--- assets/behaviours/items/collectable.js
+++ assets/behaviours/items/collectable.js
@@ -5,16 +5,18 @@
 
 var Collectable = function(_gameobject) {	
 	LR.Behaviour.Trigger.call(this,_gameobject);
+	this.collected = false;
 }
 
 Collectable.prototype = Object.create(LR.Behaviour.Trigger.prototype);
 Collectable.prototype.constructor = Collectable;
 
 Collectable.prototype.onBeginContact = function(_otherBody, _myShape, _otherShape, _equation){
-	if( _otherBody.go.layer == "player"){
+	if( this.collected == false && _otherBody.go.layer == "player"){
 		var player = _otherBody.go.getBehaviour(Player);
 		if( player && !player.dead){
 			this.onCollected();
+			this.collected = true;
 			LR.Behaviour.Trigger.prototype.onBeginContact.call(this,_otherBody, _myShape, _otherShape, _equation);			
 		}
 	}
@@ -22,5 +24,19 @@ Collectable.prototype.onBeginContact = function(_otherBody, _myShape, _otherShap
 
 Collectable.prototype.onCollected = function(_gameobject){
 	this.go.playSound("collect");
-	this.entity.kill();
+	this.kill();
+}
+
+Collectable.prototype.kill = function(){
+	console.log("fqjs");
+	this.dead = true;
+	this.go.x = -100000;
+	this.entity.visible = false;
+}
+
+Collectable.prototype.revive = function(_x,_y){
+	this.dead = false;
+	this.entity.visible = true;
+	if( _x != null ) this.go.x = _x;
+	if( _y != null ) this.go.y = _y;
 }
--- assets/behaviours/other/loot_manager.js
+++ assets/behaviours/other/loot_manager.js
@@ -5,6 +5,7 @@
 var LootManager = function(_gameobject){
 	LR.Behaviour.call(this,_gameobject);
 	this.entity.game.pollinator.on("onEnemyDied",this.pop,this);
+	this.lootsScripts = new Array();
 }
 
 LootManager.prototype = Object.create(LR.Behaviour);
@@ -13,8 +14,10 @@ LootManager.prototype.constructor = LootManager;
 LootManager.prototype.create = function(_data){
 	this.entity.x =0; this.entity.y = 0;
 	for(var i=0; i < this.entity.children.length ; i++){
-		if(this.entity.children[i].kill)
-			this.entity.children[i].kill();
+		var child = this.entity.children[i];
+		var lootS = child.go.getBehaviour(Collectable);
+		this.lootsScripts.push(lootS);
+		lootS.kill();
 	}
 }
 
@@ -22,23 +25,32 @@ LootManager.prototype.onDestroy = function(){
 	this.entity.game.pollinator.off("onEnemyDied",this.pop,this);
 }
 
+LootManager.prototype.getDeadLoot = function(){
+	var loot = null;
+	for(var i=0; i < this.lootsScripts.length ; i++){
+		var loot = this.lootsScripts[i];
+		if( loot.dead ){
+			return loot;
+		}
+	}
+	return null;
+}
+
 LootManager.prototype.pop = function(_data){
 	if( _data.sender ){
 		var loot = null;
 		//try getting a random loot
-		var r = Math.round( Math.random() * this.entity.children.length );
-		loot = this.entity.children[r];
+		var r = Math.round( Math.random() * (this.lootsScripts.length-1) );
+		loot = this.lootsScripts[r];
+		console.log(loot);
 		//if it fails, get the first loot available
-		if( loot == null || loot.alive == true )
-			loot = this.entity.getFirstDead();
+		if( loot == null || loot.dead == false )
+			loot = this.getDeadLoot();
 		//put the loot at the sender's position
 		if( loot != null ){
-			var bh = loot.go.getBehaviour(Collectable);
-			if( bh )
-				bh.respawnable = true;
-			loot.revive();
-			loot.body.x = _data.sender.world.x;
-			loot.body.y = _data.sender.world.y;
+			loot.respawnable = true;
+			//respawn
+			loot.revive(_data.sender.world.x,_data.sender.world.y);
 		}
 	}
 }
